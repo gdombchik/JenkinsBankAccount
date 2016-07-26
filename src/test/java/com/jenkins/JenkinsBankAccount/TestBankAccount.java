57430f454500ab01c2835e8b71ddbb5e5638ab6a
Gregory Dombchik <gbdjobs1@gmail.com>
2016-07-26 18:46:26 -0400

    Add helloWorld test

--- src/test/java/com/jenkins/JenkinsBankAccount/TestBankAccount.java
+++ src/test/java/com/jenkins/JenkinsBankAccount/TestBankAccount.java
@@ -1,7 +1,7 @@
 package com.jenkins.JenkinsBankAccount;
 
-import org.junit.Test;
 import org.junit.Assert;
+import org.junit.Test;
 
 public class TestBankAccount {
 
@@ -17,6 +17,11 @@ public class TestBankAccount {
 		BankAccount bankAccount = new BankAccount(10.0);
 		double amount = bankAccount.debit(11.0);
 		Assert.assertTrue(amount==10.0);
+	}
+	
+	@Test
+	public void helloWorld(){
+		System.out.println("Hello World");
 	}
 	
 }
