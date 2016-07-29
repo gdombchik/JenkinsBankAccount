53b9f01680733d50dc84bdcf841963859b968c8c
Gregory Dombchik <gbdjobs1@gmail.com>
2016-07-29 19:30:44 -0400

    Add PhantomJSDriver

--- src/test/java/com/jenkins/JenkinsBankAccount/TestBankAccount.java
+++ src/test/java/com/jenkins/JenkinsBankAccount/TestBankAccount.java
@@ -3,6 +3,11 @@ package com.jenkins.JenkinsBankAccount;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.io.File;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriver;
+
 public class TestBankAccount {
 
 	@Test
@@ -24,4 +29,14 @@ public class TestBankAccount {
 		System.out.println("Hello World!!!!");
 	}
 	
+	@Test
+	public void phantomJSDriverTest(){
+		//     				/usr/local/share/
+		File src = new File("/Users/gregorydombchik/Downloads/phantomjs-2.1.1-macosx/bin/phantomjs");
+		//File src = new File("/usr/local/share/phantomjs");
+	    System.setProperty("phantomjs.binary.path", src.getAbsolutePath());
+	    WebDriver webdriver = new PhantomJSDriver();
+	    webdriver.navigate().to("http://www.thetestroom.com/webapp/");
+	    Assert.assertTrue(webdriver.getTitle().equals("Zoo Adoption | Home"));
+	}
 }
